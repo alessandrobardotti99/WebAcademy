1e8b79b2abd6b4e1ced681649e6dcfddf992a973
Alessandro <alessandro.accomazzi@gmail.comm>
2024-07-10 18:33:28 +0200

    avanzamento

--- src/components/CentralSectionHomepage.vue
+++ src/components/CentralSectionHomepage.vue
@@ -1,10 +1,19 @@
 <script>
 import IconaObbiettivi from '../components/icons/IconaObbiettiviHomepage.vue'
+import iconaCoppa from '../components/icons/IconaCoppa.vue'
+import IconaTriste from '../components/icons/IconaTriste.vue'
 
 export default {
-  components: {
-    IconaObbiettivi
-  }
+    components: {
+        IconaObbiettivi,
+        iconaCoppa,
+        IconaTriste
+    },
+    mounted() {
+        this.$nextTick(() => {
+            MotionPlugin.install(this.$app)
+        })
+    }
 }
 </script>
 
@@ -16,20 +25,29 @@ export default {
             aiutarti a padroneggiare i fondamenti di HTML, CSS e JavaScript</p>
     </div>
     <div class="w-full">
-        <div class="w-[80%] m-auto mt-24 flex gap-4 items-center">
+        <div class="w-[80%] m-auto mt-24 flex gap-16 items-center">
             <div class="w-[40%]">
-                <p class="text-neutral-500 text-xl leading-8">Immergiti nella nostra vasta libreria di corsi online e libera il tuo
-                    potenziale come sviluppatore web. <br> Dal principiante all'avanzato, il nostro curriculum copre tutto
+                <p class="text-neutral-500 text-xl leading-8 animated-icon-triste">
+                    Immergiti nella nostra vasta libreria di corsi online e libera il tuo
+                    potenziale come sviluppatore web. <br> Da principiante ad avanzato.
                 </p>
+                <div
+                    class="flex items-center justify-between bg-gradient-to-r from-[40%] from-YellowWebAcademy to-indigo-600 rounded-xl p-4 mt-8 shadow-lg">
+                    <IconaTriste class="animated-icon-triste" />
+                    <iconaCoppa class="animated-icon-coppa" />
+                </div>
             </div>
-            <div class="bg-gradient-to-r from-30% from-black to-indigo-600 w-[60%] px-16 pt-8 pb-8 rounded-xl">
+            <div class="bg-gradient-to-r from-30% from-black to-indigo-600 w-[60%] px-16 pt-8 pb-8 rounded-xl animated-icon-coppa">
                 <div class="w-full flex justify-between items-center">
-                <span class="text-sm text-white">Costruisci siti web reattivi e interattivi</span> 
-                <button class="bg-transparent text-whie py-2 px-4 text-white rounded-xl cursor-pointer border border-white hover:bg-indigo-800 ">Iniziamo!</button>
-            </div>
+                    <span class="text-sm text-white">Costruisci siti web reattivi e interattivi</span>
+                    <button
+                        class="bg-transparent text-whie py-2 px-4 text-white rounded-xl cursor-pointer border border-white hover:bg-indigo-800 ">Iniziamo!</button>
+                </div>
                 <h2 class="text-white text-[2rem]">Realizza il tuo <br> sogno di sviluppatore </h2>
                 <p class="text-neutral-500 mt-4">Massimizza il tuo potenziale</p>
-                <p class="text-indigo-500 mt-4 text-sm flex items-center gap-2">Raggiungi i tuoi obbiettivi <span><IconaObbiettivi/></span></p>
+                <p class="text-indigo-500 mt-4 text-sm flex items-center gap-2">Raggiungi i tuoi obbiettivi <span>
+                        <IconaObbiettivi />
+                    </span></p>
             </div>
         </div>
     </div>
@@ -39,5 +57,37 @@ export default {
 <style scoped>
 .font-monospace {
     font-family: monospace;
+}
+
+@keyframes slide-in-left {
+    from {
+        transform: translateX(-100%);
+        opacity: 0;
+    }
+
+    to {
+        transform: translateX(0);
+        opacity: 1;
+    }
+}
+
+@keyframes slide-in-right {
+    from {
+        transform: translateX(100%);
+        opacity: 0;
+    }
+
+    to {
+        transform: translateX(0);
+        opacity: 1;
+    }
+}
+
+.animated-icon-triste {
+    animation: slide-in-left 0.5s ease-out forwards;
+}
+
+.animated-icon-coppa {
+    animation: slide-in-right 1s ease-out forwards;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { MotionPlugin } from '@vueuse/motion'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+app.use(MotionPlugin)
 
 app.use(createPinia())
 app.use(router)
