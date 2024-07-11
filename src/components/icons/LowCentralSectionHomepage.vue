238e117f96cebb11409caee77f6cc9697a165a35
Alessandro <alessandro.accomazzi@gmail.comm>
2024-07-11 15:32:02 +0200

    avanzamento

--- src/components/icons/LowCentralSectionHomepage.vue
+++ src/components/icons/LowCentralSectionHomepage.vue
@@ -23,24 +23,45 @@ export default {
         <div class="grid grid-cols-3 w-full m-auto mt-16 gap-16">
             <div>
                 <div class="w-full bg-indigo-600 rounded-xl p-4">
-                    <IconaStudio  class="m-auto"/>
+                    <IconaStudio class="m-auto" />
                     <div class="mt-8">
-                        <p class="text-center text-white">Impara a creare layout accattivanti e funzionali con le ultime tecniche e best practice.</p>
+                        <p class="text-center text-white">Impara a creare layout accattivanti e funzionali con le ultime
+                            tecniche e best practice.</p>
                     </div>
                 </div>
             </div>
             <div class="w-full bg-indigo-600 rounded-xl p-4">
                 <IconaPerformance class="m-auto" />
                 <div class="mt-8">
-                        <p class="text-center text-white">Ottimizza i tuoi siti web per garantire un'esperienza utente fluida e veloce.</p>
-                    </div>
+                    <p class="text-center text-white">Ottimizza i tuoi siti web per garantire un'esperienza utente
+                        fluida e veloce.</p>
+                </div>
             </div>
             <div class="w-full bg-indigo-600 rounded-xl p-4">
                 <IconaStar class="m-auto" />
                 <div class="mt-8">
-                        <p class="text-center text-white">Scopri gli strumenti più utili e le risorse indispensabili per ogni sviluppatore web.</p>
-                    </div>
+                    <p class="text-center text-white">Scopri gli strumenti più utili e le risorse indispensabili per
+                        ogni sviluppatore web.</p>
+                </div>
             </div>
+        </div>
+    </div>
+
+    <div class="mt-24 flex items-center justify-center w-[80%] m-auto">
+        <div class="w-[50%]">
+            <h1 class="font-monospace text-[2rem] text-center">Studi o lavori?</h1>
+            <p class="text-center">Puoi frequentare il corso quando vuoi, è strutturato con lezioni registrate, perfette
+                per chi ha impegni lavorativi o di studio durante il giorno.</p>
+            <div class="m-auto text-center">
+                <router-link to="/accedi">
+                    <button
+                        class="bg-YellowWebAcademy text-neutral-800 mt-4 py-2 px-4 rounded-xl cursor-pointer hover:bg-HoverYwlloWebAcademt m-auto">Inizia
+                        subito!</button>
+                </router-link>
+            </div>
+        </div>
+        <div class="w-[50%] m-auto">
+            <img src="/src/assets/img/Animazione_2.gif" alt="gif" class="h-auto max-w-full w-[600px] m-auto">
         </div>
     </div>
 </template>
