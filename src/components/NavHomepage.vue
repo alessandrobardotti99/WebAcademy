723c54184704c47e4bb531a3273b170cb0a453e6
Alessandro <alessandro.accomazzi@gmail.comm>
2024-07-11 02:19:18 +0200

    sistemazione Auth cookie

--- src/components/NavHomepage.vue
+++ src/components/NavHomepage.vue
@@ -18,11 +18,21 @@
           <li class="text-neutral-500 py-2 px-4 rounded-xl hover:text-black cursor-pointer">
             Contatti
           </li>
-          <router-link to="/accedi">
-          <li class="bg-indigo-600 text-whie py-2 px-4 text-white rounded-xl cursor-pointer hover:bg-indigo-700">
-            Accedi
-          </li>
-          </router-link>
+          <template v-if="user">
+            <li class="text-neutral-500 py-2 px-4 rounded-xl cursor-pointer">
+              Ciao, {{ user.email }}
+            </li>
+            <li @click="logout" class="text-neutral-500 py-2 px-4 rounded-xl cursor-pointer">
+              Logout
+            </li>
+          </template>
+          <template v-else>
+            <router-link to="/accedi">
+              <li class="bg-indigo-600 text-whie py-2 px-4 text-white rounded-xl cursor-pointer hover:bg-indigo-700">
+                Accedi
+              </li>
+            </router-link>
+          </template>
         </ul>
       </div>
     </nav>
@@ -30,27 +40,41 @@
 </template>
 
 <script>
-import { ref, onMounted, onBeforeUnmount } from 'vue';
+import { ref, onMounted, onBeforeUnmount } from 'vue'
+import Cookies from 'js-cookie'
 
 export default {
   setup() {
-    const hasScrolled = ref(false);
+    const hasScrolled = ref(false)
+    const user = ref(null)
 
     const handleScroll = () => {
-      hasScrolled.value = window.scrollY > 0;
-    };
+      hasScrolled.value = window.scrollY > 0
+    }
+
+    const logout = () => {
+      Cookies.remove('supabaseSession')
+      user.value = null
+      window.location.href = '/'
+    }
 
     onMounted(() => {
-      window.addEventListener('scroll', handleScroll);
-    });
+      window.addEventListener('scroll', handleScroll)
+      const session = Cookies.get('supabaseSession')
+      if (session) {
+        user.value = JSON.parse(session).user
+      }
+    })
 
     onBeforeUnmount(() => {
-      window.removeEventListener('scroll', handleScroll);
-    });
+      window.removeEventListener('scroll', handleScroll)
+    })
 
     return {
-      hasScrolled
-    };
+      hasScrolled,
+      user,
+      logout
+    }
   }
-};
+}
 </script>
