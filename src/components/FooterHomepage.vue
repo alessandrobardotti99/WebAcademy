b68a7e3b00ec36ffeb6b68fe2cd814f78e8de0ee
Alessandro <alessandro.accomazzi@gmail.comm>
2024-07-18 17:44:35 +0200

    avanzamento

--- src/components/FooterHomepage.vue
+++ src/components/FooterHomepage.vue
@@ -1,6 +1,6 @@
 <template>
     <footer>
-        <div class="w-full mt-24 bg-slate-200 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 px-8 md:px-16 lg:px-36 py-16 border-t-4 border border-t-gray-800">
+        <div class="w-full mt-24 bg-white grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 px-8 md:px-16 lg:px-36 py-16 border-t-4 border border-t-gray-800">
             <div>
                 <img src="/src/assets/img/logoWebAcademy-removebg-preview.png" alt="logoWebAcademy" class="max-w-full h-[30px] mb-4">
                 <p>Formazione digitale a 360 gradi. Corsi, tutorial, notizie e roadmap da seguire.</p>
@@ -27,7 +27,7 @@
                 </ul>
             </div>
         </div>
-        <div class="w-full border-t-2 border border-t-gray-800 border-slate-500 bg-slate-200 p-4">
+        <div class="w-full border-t-2 border border-t-gray-800 border-slate-500 bg-white p-4">
             <p class="text-center text-black">Copyright © 2024 WebAcademy.it</p>
         </div>
     </footer>
